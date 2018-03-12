d5752a068d0a03968e532731e0780082ac044f25
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-03-12 16:18:18 +0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './source/index.js',
+    output: {
+        path: __dirname + '/dist/assets',
+        filename: 'bundle.js',
+        sourceMapFilename: 'bundle.map',
+    },
+    devtool: '#source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: ['babel-loader'],
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            warnings: false,
+            mangle: true
+        }),
+    ]
+};
